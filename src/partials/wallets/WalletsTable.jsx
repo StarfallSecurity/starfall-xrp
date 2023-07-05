1e8ad4a2ccd3e92635b3fc437a64187154bac204
kumarparth380 <kumarparth380@gmail.com>
2023-07-05 18:55:58 +0530

    feat: add wallets table

---
+++ src/partials/wallets/WalletsTable.jsx
@@ -0,0 +1,111 @@
+import React, { useState, useEffect } from 'react';
+import Wallet from './WalletsTableItem';
+
+import Image01 from '../../images/icon-01.svg';
+import Image02 from '../../images/icon-02.svg';
+import Image03 from '../../images/icon-03.svg';
+
+import { useWallets } from '../../hooks/useWallets';
+
+function WalletsTable({
+  selectedItems
+}) {
+  const {data} = useWallets()
+  const [selectAll, setSelectAll] = useState(false);
+  const [isCheck, setIsCheck] = useState([]);
+
+  const handleSelectAll = () => {
+    setSelectAll(!selectAll);
+    setIsCheck(list.map(li => li.id));
+    if (selectAll) {
+      setIsCheck([]);
+    }
+  };
+
+  const handleClick = e => {
+    const { id, checked } = e.target;
+    setSelectAll(false);
+    setIsCheck([...isCheck, id]);
+    if (!checked) {
+      setIsCheck(isCheck.filter(item => item !== id));
+    }
+  };
+
+  useEffect(() => {
+    selectedItems(isCheck);
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [isCheck]);
+
+  return (
+    <div className="bg-white shadow-lg rounded-sm border border-slate-200 relative">
+      <header className="px-5 py-4">
+        <h2 className="font-semibold text-slate-800">All Wallets <span className="text-slate-400 font-medium">{data?.length}</span></h2>
+      </header>
+      <div>
+
+        {/* Table */}
+        <div className="overflow-x-auto">
+          <table className="table-auto w-full">
+            {/* Table header */}
+            <thead className="text-xs font-semibold uppercase text-slate-500 bg-slate-50 border-t border-b border-slate-200">
+              <tr>
+                <th className="px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap w-px">
+                  <div className="flex items-center">
+                    <label className="inline-flex">
+                      <span className="sr-only">Select all</span>
+                      <input className="form-checkbox" type="checkbox" checked={selectAll} onChange={handleSelectAll} />
+                    </label>
+                  </div>
+                </th>
+                <th className="px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap w-px">
+                  <span className="sr-only">Favourite</span>
+                </th>
+                <th className="px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap">
+                  <div className="font-semibold text-left">Wallet Address</div>
+                </th>
+                <th className="px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap">
+                  <div className="font-semibold text-left">Join Date</div>
+                </th>
+                <th className="px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap">
+                  <div className="font-semibold text-left">Status</div>
+                </th>
+                <th className="px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap">
+                  <div className="font-semibold">Risk Score</div>
+                </th>
+                <th className="px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap">
+                  <div className="font-semibold text-left">Action</div>
+                </th>
+                <th className="px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap">
+                  <span className="sr-only">Menu</span>
+                </th>
+              </tr>
+            </thead>
+            {/* Table body */}
+            <tbody className="text-sm divide-y divide-slate-200">
+              {
+                data?.map(wallet => {
+                  const {id, address, created, user} = wallet || {}
+                  return (
+                    <Wallet
+                      key={id}
+                      id={id}
+                      address={address?.hash}
+                      isBlackList={address?.is_blacklist}
+                      created={created}
+                      handleClick={handleClick}
+                      isChecked={isCheck.includes(id)}
+                      image={Image01}
+                    />
+                  )
+                })
+              }
+            </tbody>
+          </table>
+
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default WalletsTable;
---
+++ src/partials/wallets/WalletsTableItem.jsx
@@ -0,0 +1,49 @@
+import React, { useState } from 'react';
+
+function WalletsTableItem(props) {
+    const {address, isBlackList, created,handleClick, isChecked} = props || {}
+
+  const [open, setOpen] = useState(false);
+
+  return (
+    <tr>
+      <td className="px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap w-px">
+        <div className="flex items-center">
+          <label className="inline-flex">
+            <span className="sr-only">Select</span>
+            <input id={props.id} className="form-checkbox" type="checkbox" onChange={handleClick} checked={isChecked} />
+          </label>
+        </div>
+      </td>
+      <td className="px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap">
+          <div className="flex items-center text-slate-800">
+            <div className="w-10 h-10 shrink-0 flex items-center justify-center bg-slate-100 rounded-full mr-2 sm:mr-3">
+              <img className="ml-1" src={props.image} width="20" height="20" alt={props.order} />
+            </div>
+            <div className="font-medium text-sky-500">{props.order}</div>
+          </div>
+      </td>
+      <td className="px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap">
+        <div className="text-left">{address}</div>
+      </td>
+      <td className="px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap">
+        <div className="text-left">{created}</div>
+      </td>
+      <td className="px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap">
+        <div className="text-center">{props.orders}</div>
+      </td>
+      <td className="px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap">
+        <div className="text-left font-medium text-sky-500">{props.lastOrder}</div>
+      </td>
+      <td className="px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap">
+        <div className="text-left font-medium text-emerald-500">{props.spent}</div>
+      </td>
+      <td className="px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap">
+        <div className="text-center">{props.refunds}</div>
+      </td>
+      
+    </tr>
+  );
+}
+
+export default WalletsTableItem;
