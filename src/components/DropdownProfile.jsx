31ab4d596f71cba1537af88575b4791ecadec3f0
kumarparth380 <kumarparth380@gmail.com>
2023-07-14 19:10:19 +0530

    feat: handle logout

--- src/components/DropdownProfile.jsx
+++ src/components/DropdownProfile.jsx
@@ -1,13 +1,13 @@
 import React, { useState, useRef, useEffect } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import Transition from '../utils/Transition';
 
 import UserAvatar from '../images/user-avatar-32.png';
+import useToken from '../hooks/useToken';
 
-function DropdownProfile({
-  align
-}) {
-
+function DropdownProfile({ align }) {
+  const navigate = useNavigate();
+  const { deleteToken } = useToken();
   const [dropdownOpen, setDropdownOpen] = useState(false);
 
   const trigger = useRef(null);
@@ -17,7 +17,8 @@ function DropdownProfile({
   useEffect(() => {
     const clickHandler = ({ target }) => {
       if (!dropdown.current) return;
-      if (!dropdownOpen || dropdown.current.contains(target) || trigger.current.contains(target)) return;
+      if (!dropdownOpen || dropdown.current.contains(target) || trigger.current.contains(target))
+        return;
       setDropdownOpen(false);
     };
     document.addEventListener('click', clickHandler);
@@ -34,6 +35,12 @@ function DropdownProfile({
     return () => document.removeEventListener('keydown', keyHandler);
   });
 
+  const handleLogout = () => {
+    setDropdownOpen(!dropdownOpen);
+    deleteToken();
+    navigate('/signin');
+  };
+
   return (
     <div className="relative inline-flex">
       <button
@@ -45,7 +52,9 @@ function DropdownProfile({
       >
         <img className="w-8 h-8 rounded-full" src={UserAvatar} width="32" height="32" alt="User" />
         <div className="flex items-center truncate">
-          <span className="truncate ml-2 text-sm font-medium group-hover:text-slate-800">Acme Inc.</span>
+          <span className="truncate ml-2 text-sm font-medium group-hover:text-slate-800">
+            Acme Inc.
+          </span>
           <svg className="w-3 h-3 shrink-0 ml-1 fill-current text-slate-400" viewBox="0 0 12 12">
             <path d="M5.9 11.4L.5 6l1.4-1.4 4 4 4-4L11.3 6z" />
           </svg>
@@ -53,7 +62,9 @@ function DropdownProfile({
       </button>
 
       <Transition
-        className={`origin-top-right z-10 absolute top-full min-w-44 bg-white border border-slate-200 py-1.5 rounded shadow-lg overflow-hidden mt-1 ${align === 'right' ? 'right-0' : 'left-0'}`}
+        className={`origin-top-right z-10 absolute top-full min-w-44 bg-white border border-slate-200 py-1.5 rounded shadow-lg overflow-hidden mt-1 ${
+          align === 'right' ? 'right-0' : 'left-0'
+        }`}
         show={dropdownOpen}
         enter="transition ease-out duration-200 transform"
         enterStart="opacity-0 -translate-y-2"
@@ -82,19 +93,23 @@ function DropdownProfile({
               </Link>
             </li>
             <li>
-              <Link
+              {/* <Link
                 className="font-medium text-sm text-indigo-500 hover:text-indigo-600 flex items-center py-1 px-3"
                 to="/signin"
                 onClick={() => setDropdownOpen(!dropdownOpen)}
+              > */}
+              <button
+                onClick={handleLogout}
+                className="font-medium text-sm text-indigo-500 hover:text-indigo-600 flex items-center py-1 px-3"
               >
                 Sign Out
-              </Link>
+              </button>
             </li>
           </ul>
         </div>
       </Transition>
     </div>
-  )
+  );
 }
 
 export default DropdownProfile;
