4e55f308f29be7b5f7bb5e97aa6cd044a60659b5
kumarparth380 <kumarparth380@gmail.com>
2023-08-02 21:47:52 +0530

    fix: risk score to two digit & no prediction placeholder

--- src/partials/wallets/WalletsTableItem.jsx
+++ src/partials/wallets/WalletsTableItem.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { getFormattedDate, typeIcon } from '../../utils/Utils';
+import { getFormattedDate, toFixed, typeIcon } from '../../utils/Utils';
 import { fetchWalletPrediction } from '../../services/network/wallet';
 import { Link } from 'react-router-dom';
 
@@ -63,13 +63,12 @@ function WalletsTableItem(props) {
               </div>
             </div>
             <div className="text-left">
-            <Link
+              <Link
                 to={`/dashboard/wallet/${address}`}
                 onClick={() => setDropdownOpen(!dropdownOpen)}
               >
                 <span>{address}</span>
-            </Link>
-              
+              </Link>
             </div>
           </div>
         </td>
@@ -124,17 +123,25 @@ function WalletsTableItem(props) {
         <td colSpan="10" className="px-2 first:pl-5 last:pr-5 py-3">
           <div className="bg-slate-50 p-3 -mt-3">
             <div className="flex px-3">
-              <svg
-                className="w-4 h-4 fill-current text-slate-500 shrink-0 mr-2 mt-1"
-                viewBox="0 0 16 16"
-              >
-                <path d="M11.7.3c-.4-.4-1-.4-1.4 0l-10 10c-.2.2-.3.4-.3.7v4c0 .6.4 1 1 1h4c.3 0 .5-.1.7-.3l10-10c.4-.4.4-1 0-1.4l-4-4zM4.6 14H2v-2.6l6-6L10.6 8l-6 6zM12 6.6L9.4 4 11 2.4 13.6 5 12 6.6z" />
-              </svg>
-              <div className="w-full justify-between text-sm text-slate-600 italic">
-                <div>Risk Score: {predictionData?.fraud_probability}</div>
-                <div className="mt-1">Message: {predictionData?.fraud_message}</div>
-                <div className="mt-1">Reason: {predictionData?.fraud_message}</div>
-              </div>
+              {predictionData ? (
+                <>
+                  <svg
+                    className="w-4 h-4 fill-current text-slate-500 shrink-0 mr-2 mt-1"
+                    viewBox="0 0 16 16"
+                  >
+                    <path d="M11.7.3c-.4-.4-1-.4-1.4 0l-10 10c-.2.2-.3.4-.3.7v4c0 .6.4 1 1 1h4c.3 0 .5-.1.7-.3l10-10c.4-.4.4-1 0-1.4l-4-4zM4.6 14H2v-2.6l6-6L10.6 8l-6 6zM12 6.6L9.4 4 11 2.4 13.6 5 12 6.6z" />
+                  </svg>
+                  <div className="w-full justify-between text-sm text-slate-600 italic">
+                    <div>Risk Score: {toFixed(predictionData?.fraud_probability)}</div>
+                    <div className="mt-1">Message: {predictionData?.fraud_message}</div>
+                    <div className="mt-1">Reason: {predictionData?.fraud_message}</div>
+                  </div>
+                </>
+              ) : (
+                <div className="w-full justify-between text-sm text-slate-600 italic">
+                  No prediction found
+                </div>
+              )}
             </div>
           </div>
         </td>
